eb1182dd46aa075dfde9d004aeaef25f2f3e58e7
manue <manuelvaalram@gmail.com>
2024-09-03 22:04:38 -0400

    init repo

---
+++ client/src/components/ResumeTitle.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-card variant="text">
+    <v-avatar
+      image="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      size="100"
+    ></v-avatar>
+    <v-card-title class="text-h3 text-wrap">Hola, soy Manuel Valenzuela</v-card-title>
+    <v-card-subtitle class="text-h2 text-wrap text-caption" >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Accusantium animi consequatur dicta
+      dolorem ea id iste iure natus nihil omnis quaerat quas quasi quibusdam quisquam rerum saepe, ut veritatis
+      voluptatum.
+    </v-card-subtitle>
+    <v-card-actions>
+      <v-chip
+        prepend-icon="mdi-email"
+        href="#"
+      >
+        Contáctame
+      </v-chip>
+      <v-chip
+        prepend-icon="mdi-linkedin"
+        href=""
+      >
+        Linkedin
+      </v-chip>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ client/src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container class="px-16" max-width="1000">
+    <ResumeTitle/>
+  </v-container>
+</template>
+
+<script setup>
+</script>
